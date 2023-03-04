8b0c6acb8158cd4345f038c6bffc1f49c90b3962
raushan8586 <72825046+raushan8586@users.noreply.github.com>
2023-03-04 09:43:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.57.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA4LM34PDXWM427JNR"
+  secret_key = "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"
+}
+
+//Create VPC
+resource "aws_vpc" "edureka-vpc" {
+  cidr_block       = "192.168.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "edureka-vpc"
+  }
+}
+
+//Create Subnets
+resource "aws_subnet" "edureka-subnet" {
+  vpc_id     = aws_vpc.edureka-vpc.id
+  cidr_block = "192.168.0.0/24"
+
+  tags = {
+    Name = "edureka-subnet"
+  }
+}
+
+//Create Internet Gateway
+resource "aws_internet_gateway" "edureka-igw" {
+  vpc_id = aws_vpc.edureka-vpc.id
+
+  tags = {
+    Name = "edureka-igw"
+  }
+}
